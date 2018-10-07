d779d8d2cc84efcc055d43dd785cc13862234d48
Roy Segall <roy@segall.io>
2018-10-07 22:36:18 +0300

    Start to skitch

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="upper">
+      <input name="income" type="number" placeholder="Income" />
+      Balance: 5,000
+      Total: 35,000
+    </div>
+
+    <div>
+      <div class="title"><input type="text" placeholder="title" /></div>
+      <div class="expenses">
+        <div class="expense">
+          <div class="title"><input type="text" placeholder="Title"/></div>
+          <div class="value"><input type="number" placeholder="Value"/></div>
+        </div>
+      </div>
+    </div>
+
+    <div class="actions">
+      Total: 35,000
+
+      <button>Add another section</button>
+      <button>Export to file</button>
+      <button>Login and save</button>
+    </div>
   </div>
 </template>
 
+<style lang="scss">
+  .home {
+
+  }
+</style>
+
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Component({
-  components: {
-    HelloWorld,
-  },
 })
 export default class Home extends Vue {}
 </script>
