9f11b837c83e7d26c7dc7ec3c717b06eae1fa28e
Roy Segall <roy@segall.io>
2018-10-08 23:06:59 +0300

    More code.

--- front/src/App.vue
+++ front/src/App.vue
@@ -5,18 +5,30 @@
 </template>
 
 <style lang="scss">
+
+
+body {
+  background-color: #ffffff;
+  font-size: 16px;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  padding: 0.5em;
+  font-family: 'Rubik', sans-serif, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $blue5;
+  margin: 1em;
 }
+
 #nav {
   padding: 30px;
+
   a {
     font-weight: bold;
     color: #2c3e50;
+
     &.router-link-exact-active {
       color: #42b983;
     }
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,35 +1,89 @@
 <template>
   <div class="home">
-    <div class="upper">
-      <input name="income" type="number" placeholder="Income" />
-      Balance: 5,000
-      Total: 35,000
-    </div>
-
-    <div>
-      <div class="title"><input type="text" placeholder="title" /></div>
-      <div class="expenses">
-        <div class="expense">
-          <div class="title"><input type="text" placeholder="Title"/></div>
-          <div class="value"><input type="number" placeholder="Value"/></div>
-        </div>
+    <div class="row upper">
+      <div class="col-6 text-left">
+        <input name="income" type="number" placeholder="Income" class="form-control" />
+      </div>
+      <div class="col-6 text-right">
+        Balance: 5,000
+        Total: 35,000
       </div>
     </div>
 
-    <div class="actions">
-      Total: 35,000
+    <div class="row">
+      <div class="block col-6">
 
-      <button>Add another section</button>
-      <button>Export to file</button>
-      <button>Login and save</button>
+        <div class="block-header">
+          <div class="title"><input type="text" placeholder="Enter title here" class="form-control title" /></div>
+        </div>
+
+        <div class="expenses">
+          <div class="expense row">
+            <div class="title col-md-6"><input type="text" placeholder="Title" class="form-control" /></div>
+            <div class="value col-md-6"><input type="number" placeholder="Value" class="form-control" /></div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="actions row">
+      <div class="col-8 text-left">
+        <button type="button" class="btn btn-primary">Add another section</button>
+        <button type="button" class="btn btn-light">Export to file</button>
+        <button type="button" class="btn btn-success">Login and save</button>
+      </div>
+
+      <div class="col-4 text-right">
+        Total: 25,000
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss">
+
   .home {
 
+    .block {
+      padding: 0;
+      border: solid 1px $blue1;
+      background: $white2;
+      margin: 1em;
+
+      input {
+        margin: 0;
+        border-radius: 0;
+        border: none;
+
+        &.title {
+          text-align: center;
+          border-bottom: solid 1px $blue2;
+          color: $blue3;
+        }
+      }
+
+      .expense {
+
+        .title {
+          padding-right: 0;
+          border-right: $blue1 solid 1px;
+        }
+
+        .value {
+          padding-left: 0;
+        }
+      }
+    }
+
+    .actions {
+
+      button {
+        margin-right: .5em;
+      }
+    }
   }
+
 </style>
 
 <script lang="ts">
