4096138f53dec7f990360b61e216927b98ecbe11
Roy Segall <roy@segall.io>
2018-10-27 22:54:29 +0300

    Adding the income boilerplate

---
+++ backend/src/Controller/Incomes/Incomes.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller\Incomes;
+
+use App\Controller\AbstractTahiniController;
+use App\Entity\UserDefault;
+use App\Services\TahiniAccessToken;
+use App\Services\TahiniDoctrine;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Handle the user default - income and template.
+ *
+ * @Route("/api/incomes")
+ */
+class Incomes extends AbstractTahiniController
+{
+    /**
+     * Get all the incomes of the user.
+     *
+     * @return JsonResponse
+     */
+    public function getAll() {
+        return $this->json([]);
+    }
+
+    /**
+     * Adding an income.
+     *
+     * @return JsonResponse
+     */
+    public function add() {
+        // Check if we have an open income.
+        if (true) {
+            // close the current income.
+            return $this->json([]);
+        }
+
+        // Add the income.
+        return $this->json([]);
+    }
+
+    /**
+     * Delete a given income.
+     *
+     * @return JsonResponse
+     */
+    public function delete() {
+        return $this->json([]);
+    }
+
+    /**
+     * Updating a given income.
+     *
+     * @return JsonResponse
+     */
+    public function update() {
+        return $this->json([]);
+    }
+}
