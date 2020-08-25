8441bcea59853018a18902475542fff084438cb4
RoySegall <roy@segall.io>
2020-08-25 23:46:35 +0300

    Updates

---
+++ precache-manifest.ac801900653d0bf742ded3f505ddee57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64c64be1664d46be468802fca8308f65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e505e24c3fc9af50dc99",
+    "url": "/static/css/main.9dcf7b99.chunk.css"
+  },
+  {
+    "revision": "beb26704298d303e6ff4",
+    "url": "/static/js/2.d3920b0d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d3920b0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e505e24c3fc9af50dc99",
+    "url": "/static/js/main.191737d9.chunk.js"
+  },
+  {
+    "revision": "23f7e4d2c8397ce55bf6",
+    "url": "/static/js/runtime-main.5fe610d3.js"
+  }
+]);
