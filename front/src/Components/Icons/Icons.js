14978c55efe686062a342aa06c9cb5fa041327f4
Roy Segall <roy@segall.io>
2020-08-28 16:43:46 +0300

    Adding linter and ci

--- front/src/Components/Icons/Icons.js
+++ front/src/Components/Icons/Icons.js
@@ -1,24 +1,18 @@
 import React from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import {
-    faCloudUpload,
-    faCloudDownload,
-    faSignInAlt,
-    faShekelSign,
-    faPiggyBank,
-    faCreditCardFront,
-    faAbacus, faMoneyBillAlt
-} from "@fortawesome/pro-duotone-svg-icons";
+import {faMoneyBillAlt} from "@fortawesome/free-regular-svg-icons";
+
+import {faCreditCard} from "@fortawesome/free-regular-svg-icons";
+import { faCloudUploadAlt, faCloudDownloadAlt, faSignInAlt, faPiggyBank, faCalculator} from "@fortawesome/free-solid-svg-icons";
 import {faFacebookSquare, faGoogle, faApple} from "@fortawesome/free-brands-svg-icons";
 
-export const UploadCloud = () => <FontAwesomeIcon icon={faCloudUpload}/>
-export const DownloadCloud = () => <FontAwesomeIcon icon={faCloudDownload}/>
+export const UploadCloud = () => <FontAwesomeIcon icon={faCloudUploadAlt}/>
+export const DownloadCloud = () => <FontAwesomeIcon icon={faCloudDownloadAlt}/>
 export const Login = () => <FontAwesomeIcon icon={faSignInAlt}/>
 export const Facebook = () => <FontAwesomeIcon icon={faFacebookSquare}/>
 export const Google = () => <FontAwesomeIcon icon={faGoogle}/>
 export const Apple = () => <FontAwesomeIcon icon={faApple}/>
-export const Shekel = () => <FontAwesomeIcon icon={faShekelSign}/>
 export const Income = () => <FontAwesomeIcon icon={faPiggyBank}/>
-export const Expenses = () => <FontAwesomeIcon icon={faCreditCardFront}/>
-export const Calculator = () => <FontAwesomeIcon icon={faAbacus}/>
+export const Expenses = () => <FontAwesomeIcon icon={faCreditCard}/>
+export const Calculator = () => <FontAwesomeIcon icon={faCalculator}/>
 export const MoneyBill = () => <FontAwesomeIcon icon={faMoneyBillAlt}/>
