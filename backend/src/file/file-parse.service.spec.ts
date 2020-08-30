8132bbd5da674462a3cc63ada25039110728708f
RoySegall <roy@segall.io>
2020-08-30 07:21:41 +0300

    Adding more tests

--- backend/src/file/file-parse.service.spec.ts
+++ backend/src/file/file-parse.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FileParseService } from './file-parse.service';
+import path from "path";
+const FullJson = require(path.join(__dirname, '/fixture/full.json'));
 
 describe('FileParseService', () => {
   let service: FileParseService;
@@ -12,7 +14,29 @@ describe('FileParseService', () => {
     service = module.get<FileParseService>(FileParseService);
   });
 
-  it('Should have the same parsed data as the mock one', () => {
-    expect(1).toBe(1);
+  it('Should have the same parsed data as the mock one', async () => {
+    const results = await service.parseFile(path.join(__dirname, '/example_files/dummy_file.xlsx'));
+
+    expect(results).toStrictEqual(FullJson);
+  });
+
+  it('Should return the expected values for a given row', () => {
+    // service.handleRow({});
+    expect(true).toBe(true);
+  })
+
+  it('Should return the proper month name and year from a sheet title', () => {
+    const matrix = [
+      {name: '2020 דצמבר', expected: { month: 12, year: '2020' }},
+      {name: 'דצמבר 2020', expected: { month: 12, year: '2020' }},
+      {name: 'December 2020', expected: { month: 12, year: '2020' }},
+      {name: '2020 December', expected: { month: 12, year: '2020' }},
+      {name: 'Dec 2020', expected: { month: 12, year: '2020' }},
+      {name: '2020 Aug', expected: { month: 8, year: '2020' }},
+    ]
+
+    matrix.map(({name, expected}) => {
+      expect(service.getDateDataFromSheetName(name)).toStrictEqual(expected);
+    })
   });
 });
