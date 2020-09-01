da52687388bbcd78b967ae25f49bf80790b34342
RoySegall <roy@segall.io>
2020-09-01 07:46:54 +0300

    Adding modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommandModule } from 'nestjs-command';
 import { FileModule } from './file/file.module';
+import { UserModule } from './user/user.module';
+import { CategoryModule } from './category/category.module';
+import { FileRowModule } from './file-row/file-row.module';
 
 @Module({
-  imports: [FileModule, CommandModule],
+  imports: [FileModule, CommandModule, UserModule, CategoryModule, FileRowModule],
   controllers: [AppController],
   providers: [AppService],
 })
