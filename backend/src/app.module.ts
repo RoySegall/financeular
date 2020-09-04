d9a4df5f56b6313b0ef0a1966d4848dcd6af7137
RoySegall <roy@segall.io>
2020-09-04 17:58:20 +0300

    Adding the category entity

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,26 @@ import { FileModule } from './file/file.module';
 import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
 import { FileRowModule } from './file-row/file-row.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [FileModule, CommandModule, UserModule, CategoryModule, FileRowModule],
+  imports: [
+    FileModule,
+    CommandModule,
+    UserModule,
+    CategoryModule,
+    FileRowModule, TypeOrmModule.forRoot({
+        "type": "mysql",
+        "host": "localhost",
+        "port": 3306,
+        "username": "root",
+        "password": "root",
+        "database": "fincaular",
+        "autoLoadEntities": true,
+        "synchronize": true
+      }
+    )
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
