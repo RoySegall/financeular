575d32f17f499f6489d1ef4da681c54ab92dab3b
RoySegall <roy@segall.io>
2020-09-04 18:16:53 +0300

    Adding the category entity

---
+++ backend/src/category/category.entity.ts
@@ -0,0 +1,35 @@
+import {Entity, Column, PrimaryGeneratedColumn, ManyToOne} from 'typeorm';
+import {User} from "../user/user.entity";
+
+export enum Period {
+  MONTH = "month",
+  WEEK = "week",
+}
+
+@Entity()
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  amount: number;
+
+  @Column()
+  year: number;
+
+  @Column()
+  month: number;
+
+  @Column({
+    type: "enum",
+    enum: Period,
+    default: Period.WEEK,
+  })
+  period: Period;
+
+  @ManyToOne(type => User, user => user.categories)
+  user: User;
+}
