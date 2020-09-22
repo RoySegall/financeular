cb1d7ab7ea74b4e140980ec97922751124ba3452
Roy Segall <roy@segall.io>
2020-09-22 08:13:56 +0300

    #59: add more tests

--- backend/src/file/file.service.spec.ts
+++ backend/src/file/file.service.spec.ts
@@ -1,10 +1,26 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { FileService } from './file.service';
 
-describe('FileService', () => {
-  let service: FileService;
+describe('Testing the file service', () => {
+  let fileService: FileService;
+  let fileRepoMock;
+  let findMock;
 
-  it('Dummy test for now', () => {
-    expect('🍕').toBe('🍕');
+  beforeEach(() => {
+    findMock = jest.fn();
+
+    fileRepoMock = new (jest.fn(() => ({
+      find: findMock,
+    })))();
+
+    fileService = new FileService(fileRepoMock);
+  });
+
+  it('Testing the getAll method', async () => {
+    findMock.mockImplementation((args) => {
+      expect(args).toStrictEqual({relations: ['user', 'categories', 'rows']})
+      return {id: 42};
+    });
+
+    expect(await fileService.getAll()).toStrictEqual({id: 42});
   });
 });
