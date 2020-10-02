56b50afcd61bccbcc75e1b7aef78f3550ce97d8b
RoySegall <roy@segall.io>
2020-10-02 09:42:34 +0300

    Write to do when handling the access token

--- front/src/client.js
+++ front/src/client.js
@@ -1,7 +1,29 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import {ApolloClient, ApolloLink, createHttpLink, InMemoryCache, concat} from '@apollo/client';
 import {backendAddress} from "./config";
 
+const httpLink = createHttpLink({
+  uri: `${backendAddress()}/graphql`
+});
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  // todo: check if we have only one mutation - login or refresh token.
+  //  If only login - check if the access token is expires or not. If expires, then refresh the token. If not expires -
+  //  send the request. In case the token is no longer valid - remove the access token. listen to an event of local
+  //  storage changes and if any of them was remove redirect to the front page and raise a nce error to the user.
+  //  If only refresh token then continue with the request and don't do anything.
+
+  console.log('a');
+  // Add the authorization to the headers
+  operation.setContext({
+    headers: {
+      authorization: localStorage.getItem('token') || null,
+    }
+  });
+
+  return forward(operation);
+})
+
 export const client = new ApolloClient({
-  uri: `${backendAddress()}/graphql`,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  link: concat(authMiddleware, httpLink),
 });
