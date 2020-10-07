a9e222365920efcf33995021991a3ae695ad9c8d
RoySegall <roy@segall.io>
2020-10-07 08:17:31 +0300

    Fix the front page

--- front/src/Components/FrontPageBlock/FrontPageBlock.js
+++ front/src/Components/FrontPageBlock/FrontPageBlock.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default () => ({image, imageAlt, title, text, reversOrder, imageClass, className}) => <section className={`flex pl-6 pr-6 pt-10 justify-center ${reversOrder ? 'flex-row-reverse' : ''} ${className}`}>
+export default ({image, imageAlt, title, text, reversOrder, imageClass, className}) => <section className={`flex pl-6 pr-6 pt-10 justify-center ${reversOrder ? 'flex-row-reverse' : ''} ${className}`}>
   <div className="w-6/12 text-center">
     <h2 className="text-2xl pb-5">{title}</h2>
 
