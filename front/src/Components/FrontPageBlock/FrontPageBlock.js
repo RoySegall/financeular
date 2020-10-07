0c87b9abea647638dfd6677122828007c42bf214
RoySegall <roy@segall.io>
2020-10-07 08:13:28 +0300

    Split into small files

---
+++ front/src/Components/FrontPageBlock/FrontPageBlock.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default () => ({image, imageAlt, title, text, reversOrder, imageClass, className}) => <section className={`flex pl-6 pr-6 pt-10 justify-center ${reversOrder ? 'flex-row-reverse' : ''} ${className}`}>
+  <div className="w-6/12 text-center">
+    <h2 className="text-2xl pb-5">{title}</h2>
+
+    <p>{text}</p>
+  </div>
+
+  <div className="w-6/12 text-center">
+    <img alt={imageAlt} className={imageClass} src={image} />
+  </div>
+
+</section>
--- front/src/Pages/FrontPage/FrontPage.js
+++ front/src/Pages/FrontPage/FrontPage.js
@@ -2,19 +2,7 @@ import React from "react";
 import PageTitle from "../../Components/PageTitle/PageTitle";
 import research from "./research.svg"
 import personal from "./personal-settings.svg"
-
-const FrontPageBlock = ({image, imageAlt, title, text, reversOrder, imageClass, className}) => <section className={`flex pl-6 pr-6 pt-10 justify-center ${reversOrder ? 'flex-row-reverse' : ''} ${className}`}>
-  <div className="w-6/12 text-center">
-    <h2 className="text-2xl pb-5">{title}</h2>
-
-    <p>{text}</p>
-  </div>
-
-  <div className="w-6/12 text-center">
-    <img alt={imageAlt} className={imageClass} src={image} />
-  </div>
-
-</section>
+import FrontPageBlock from "../../Components/FrontPageBlock/FrontPageBlock";
 
 export default () => <div className="front-page flex flex-col pb-10">
   <PageTitle>Welcome to <b>Financular</b></PageTitle>
@@ -22,7 +10,7 @@ export default () => <div className="front-page flex flex-col pb-10">
   <FrontPageBlock
     image={research}
     imageClass="first"
-    imageAlt="Reasrching money"
+    imageAlt="Researching money"
     title="Understanding money is hard"
     text="We have the right tools to help you understand where you spent you're money"
   />
@@ -37,7 +25,7 @@ export default () => <div className="front-page flex flex-col pb-10">
     text={
       <>
         <p>Most of the tools requires you to give them information - receipts, information for logging into your bank account and more...</p>
-        <p className="pt-5">When mainting the data via excel and write down what you spent and got you can be assure that only minimal data is given with maximum feedback</p>
+        <p className="pt-5">When maintaining the data via excel and write down what you spent and got you can be assure that only minimal data is given with maximum feedback</p>
       </>
     }
   />
