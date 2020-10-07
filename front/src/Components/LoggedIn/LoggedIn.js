6cffdaf8ad2fbbe5dc613317a46c6d9b9575360f
RoySegall <roy@segall.io>
2020-10-07 08:11:10 +0300

    Minor touch

--- front/src/Components/LoggedIn/LoggedIn.js
+++ front/src/Components/LoggedIn/LoggedIn.js
@@ -20,11 +20,13 @@ export default () => {
 
   if (!data) {
     return <div className="pr-5">
-      Welcome <b>Guest</b>. Click here to <Link to="/login" className="font-bold" onClick={logOut}>login</Link>.
+      Welcome <b>Guest</b>. Click here to <Link to="/login" className="font-bold" onClick={logOut}>login</Link>
     </div>
   }
 
   return <div className="pr-5">
-    Hello <b>{data.whoAmI.username}</b>. Click here to <Link to="/logout" className="font-bold" onClick={logOut}>logout</Link> or <Link to="/dashboard" className="font-bold">View your uploaded files</Link>
+    Hello <b>{data.whoAmI.username}</b>. Click here to&nbsp;
+    <Link to="/logout" className="font-bold" onClick={logOut}>logout</Link> or&nbsp;
+    <Link to="/dashboard" className="font-bold">View your uploaded files</Link>
   </div>
 }
