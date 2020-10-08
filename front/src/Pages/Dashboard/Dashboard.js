ba2a62924a00d9cd00f65c81eb95ddad392f7972
RoySegall <roy@segall.io>
2020-10-08 17:28:49 +0300

    Workin on stuff

--- front/src/Pages/Dashboard/Dashboard.js
+++ front/src/Pages/Dashboard/Dashboard.js
@@ -2,10 +2,32 @@ import React from 'react';
 import "./Dashboard.scss"
 import PageTitle from "../../Components/PageTitle/PageTitle";
 import NoFiles from "../../Components/NoFiles/NoFiles";
+import {useQuery} from "@apollo/client";
+import {FILES} from "../../Apollo/Files";
+import loadingImage from "./loading.svg";
 
-export default () => <div className="files-wrapper">
-  <PageTitle align='left'>Your files</PageTitle>
-  <div>
-    <NoFiles />
+const DashboardFiles = ({loading, data}) => {
+
+  if (loading) {
+    return <img src={loadingImage} alt={"loading"} />
+  }
+
+  const {files} = data;
+
+  if (files.length === 0) {
+    return <NoFiles />;
+  }
+
+  return 'Files';
+}
+
+export default () => {
+  const { loading, data } = useQuery(FILES);
+
+  return <div className="files-wrapper">
+    <PageTitle align='left'>Your files</PageTitle>
+    <div>
+      <DashboardFiles loading={loading} data={data} />
+    </div>
   </div>
-</div>
+}
