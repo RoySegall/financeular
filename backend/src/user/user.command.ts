448b8bbe673cbc3a8205610ffc753933db32dc4d
Roy Segall <roy@segall.io>
2020-10-16 19:07:32 +0300

    #81: seeding users

--- backend/src/user/user.command.ts
+++ backend/src/user/user.command.ts
@@ -1,7 +1,7 @@
 import {Injectable} from "@nestjs/common";
 import {Command} from "nestjs-command";
 import {UserService} from "./user.service";
-import {AuthService} from "../auth/auth.service";
+import {User} from "./user.entity";
 
 @Injectable()
 export class UserCommand {
@@ -12,8 +12,26 @@ export class UserCommand {
     describe: 'Get the user by name',
     autoExit: true
   })
-  async dummy_stuff(
-  ) {
+  async dummy_stuff() {
     console.log(await this.userService.getByUsername('asdasdasdasdasd'));
+  }
+
+  @Command({
+    command: 'seed:users',
+    describe: 'Seeding the users',
+    autoExit: true
+  })
+  async seed_users() {
+    const users = [
+      {username: 'first_example_user', password: '1234'},
+      {username: 'second_example_user', password: '5678'},
+    ];
+
+    console.log('Start to seed users');
+
+    for (const user of users) {
+      const results = await this.userService.createUser(user.username, user.password);
+      console.log(results);
+    }
   }
 }
