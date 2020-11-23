6006409b5aeefa7599b1943b66c7e0719cd42132
RoySegall <roy@segall.io>
2020-11-23 19:16:53 +0200

    Starting to work on the excel processing

---
+++ backend/app/Console/Commands/ExcelFileProcessor.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\ExcelFileProcessorService;
+use Illuminate\Console\Command;
+use function Kint\Kint;
+
+class ExcelFileProcessor extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'excel:process';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Processing a single excel file for testing the real service';
+
+    /**
+     * @var ExcelFileProcessorService
+     */
+    protected $excelFileProcessor;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(ExcelFileProcessorService $excel_file_processor)
+    {
+        parent::__construct();
+
+        $this->excelFileProcessor = $excel_file_processor;
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $file_path = $this->ask('Please enter the file path');
+        \Kint::dump($this->excelFileProcessor->foo());
+    }
+}
---
+++ backend/app/Services/ExcelFileProcessorService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services;
+
+class ExcelFileProcessorService {
+
+    /**
+     * Handling an excel file and return the data as an array.
+     *
+     * @param $file_path
+     *   The file path.
+     *
+     * @return array The parsed data.
+     */
+    public function processFile($file_path) {
+        // Check if the path exists.
+        // Check if this an excel file.
+
+        // Read the file.
+        return [];
+    }
+
+}
