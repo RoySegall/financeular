0f7414551c616e6ce001e0d55e1b37593a78a7a5
RoySegall <roy@segall.io>
2020-12-01 08:51:18 +0200

    Adding more tests

--- backend/tests/Feature/Client/FileResolverTest.php
+++ backend/tests/Feature/Client/FileResolverTest.php
@@ -35,23 +35,84 @@ class FileResolverTest extends TestCase
     $this->firstFileExpenses = $this->createExpense($this->firstFile);
     $this->firstFileIncomes = $this->createIncome($this->firstFile);
 
-    $this->firstFileLimitations = $this->createLimitation($this->secondFile);
-    $this->firstFileExpenses = $this->createExpense($this->secondFile);
-    $this->firstFileIncomes = $this->createIncome($this->secondFile);
+    $this->secondFileLimitations = $this->createLimitation($this->secondFile);
+    $this->secondFileExpenses = $this->createExpense($this->secondFile);
+    $this->secondFileIncomes = $this->createIncome($this->secondFile);
   }
 
   /**
    * Testing all the files resolvers.
    */
   public function testGetAllFilesResolvers() {
-    $this->fail('a');
+    $query = " {
+      files {
+        data {
+          name
+          path
+          limitations { id month year value_per_week description time_per_month title }
+          expenses { id month year title value date }
+          incomes { id month year title value }
+        }
+      }
+    }";
+
+    $this->graphQL($query)->assertJson([
+      'data' => [
+        'files' => [
+          'data' => [
+            0 => [
+              'name' => $this->firstFile->name,
+              'path' => $this->firstFile->path,
+              'limitations' => [
+                0 => [
+                  'id' => $this->firstFileLimitations->id,
+                  'month' => $this->firstFileLimitations->month,
+                  'year' => $this->firstFileLimitations->year,
+                  'value_per_week' => $this->firstFileLimitations->value_per_week,
+                  'description' => $this->firstFileLimitations->description,
+                  'time_per_month' => $this->firstFileLimitations->time_per_month,
+                  'title' => $this->firstFileLimitations->title,
+                ],
+              ],
+              'expenses' => [
+                0 => ['b' => 'b'],
+              ],
+              'incomes' => [
+                0 => ['b' => 'b'],
+              ],
+            ],
+            1 => [
+              'name' => $this->secondFile->name,
+              'path' => $this->secondFile->path,
+              'limitations' => [
+                0 => [
+                  'id' => $this->firstFileLimitations->id,
+                  'month' => $this->firstFileLimitations->month,
+                  'year' => $this->firstFileLimitations->year,
+                  'value_per_week' => $this->firstFileLimitations->value_per_week,
+                  'description' => $this->firstFileLimitations->description,
+                  'time_per_month' => $this->firstFileLimitations->time_per_month,
+                  'title' => $this->firstFileLimitations->title,
+                ],
+              ],
+              'expenses' => [
+                0 => ['b' => 'b'],
+              ],
+              'incomes' => [
+                0 => ['b' => 'b'],
+              ],
+            ],
+          ],
+        ],
+      ]
+    ]);
   }
 
-  /**
-   * Testing the resolver for a single file.
-   */
-  public function testSingleFileResolver() {
-    $this->fail('a');
-  }
+//  /**
+//   * Testing the resolver for a single file.
+//   */
+//  public function testSingleFileResolver() {
+////    $this->fail('a');
+//  }
 
 }
