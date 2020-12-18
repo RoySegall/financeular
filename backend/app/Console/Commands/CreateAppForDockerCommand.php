f1985cd9e4586c7a5d0a033ba6ca5b0c40e05db4
RoySegall <roy@segall.io>
2020-12-19 00:03:29 +0200

    Adding more tests

--- backend/app/Console/Commands/CreateAppForDockerCommand.php
+++ backend/app/Console/Commands/CreateAppForDockerCommand.php
@@ -3,6 +3,10 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+use Laravel\Passport\Client;
+use Laravel\Passport\PersonalAccessClient;
 
 class CreateAppForDockerCommand extends Command
 {
@@ -11,7 +15,7 @@ class CreateAppForDockerCommand extends Command
    *
    * @var string
    */
-  protected $signature = 'financeular:create-app';
+  protected $signature = 'financeular:create-app {--client_id=} {--client_secret=}';
 
   /**
    * The console command description.
@@ -21,20 +25,46 @@ class CreateAppForDockerCommand extends Command
   protected $description = 'Creating an app. Used for services deployment.';
 
   /**
-   * Create a new command instance.
-   *
-   * @return void
-   */
-  public function __construct() {
-    parent::__construct();
-  }
-
-  /**
    * Execute the console command.
-   *
-   * @return int
    */
   public function handle() {
-    return 0;
+    try {
+      Validator::make($this->options(), ['client_id' => 'required', 'client_secret' => 'required'])
+        ->validate();
+    } catch (ValidationException $e) {
+      $this->error('The client ID or client secret are missing.');
+      return;
+    }
+
+    // Get the client id and secret as an argument.
+    [$id, $secret] = [$this->option('client_id'), $this->option('client_secret')];
+
+    // Check an application with that arguments or if a docker application exists.
+    $docker_app_exists = Client::where(['id' => $id])
+      ->orWhere(['secret' => $secret])
+      ->orWhere(['name' => 'Docker app'])
+      ->exists();
+
+    if ($docker_app_exists) {
+      $this->error('A docker app already exists');
+      return;
+    }
+
+    // Create a client and personal access token.
+    $client = new Client();
+    $client->name = 'Docker app';
+    $client->id = $id;
+    $client->secret = $secret;
+    $client->redirect = 'http://loalhost';
+    $client->personal_access_client = true;
+    $client->password_client = false;
+    $client->revoked = false;
+    $client->save();
+
+    $personal = new PersonalAccessClient();
+    $personal->client_id = $client->id;
+    $personal->save();
+
+    $this->info('The app has been created');
   }
 }
