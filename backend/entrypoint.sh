c047b174f88bbe8a030749e93cc22716a8caff75
RoySegall <roy@segall.io>
2020-12-19 15:30:46 +0200

    Minor documentation

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-# Check if DB exists.
+# Waiting for the DB to be ready. It's not enough that the container need to
+# be ready for us, we need to make sure the mysql service is ready for action.
 echo "Waiting for DB connection"
 while ! echo exit | nc -vz $MYSQL_HOST $MYSQL_PORT;
 do
@@ -9,12 +10,19 @@ do
 done
 echo "DB is online"
 
+# Preparing the passport keys and clearing the cache.
 php artisan passport:keys
+php artisan cache:clear
+
+# Caching the config and the route and create a storage link.
 php artisan config:cache
 php artisan route:cache
-php artisan cache:clear
 php artisan storage:link
+
+# Migrating with force since this is a production environment, laravel make sure
+# we don't do stupid stuff, and create a docker app.
 php artisan migrate --force
 php artisan financeular:create-app --client_id=$CLIENT_ID --client_secret=$CLIENT_SECRET
 
+# Fire up apache in the background.
 apache2-foreground
